c6cb44d7810551db99cb4e32523e369fc184d7cf
ashot <khachatryanashot1@gmail.com>
2014-04-04 00:11:45 +0400

    Implemented constructors for triangle class.

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -1,1 +1,36 @@
 #include "triangle.h"
+
+double
+Triangle::GetSideByTwoSidesAngle(double pSideA, double pSideB, double pAngle)
+{
+  (void)pSideA;
+  (void)pSideB;
+  (void)pAngle;
+  return 0;
+}
+
+Triangle::Triangle()
+{
+}
+
+Triangle::Triangle(double pSideA, double pSideB, double pSideC)
+{
+  // TODO: Check side correctness.
+  m_sideA = pSideA;
+  m_sideB = pSideB;
+  m_sideC = pSideC;
+}
+
+Triangle::Triangle(double pSideF, double pSideS, double pAngle, TrAngles pTrAngle)
+{
+  double lSideT = GetSideByTwoSidesAngle(pSideF, pSideS, pAngle);
+  if (pTrAngle == eANGLEA) {
+    Triangle(pSideF, lSideT, pSideS);
+  } else if (pTrAngle == eANGLEB) {
+    Triangle(pSideF, pSideS, lSideT);
+  } else if (pTrAngle == eANGLEC) {
+    Triangle(lSideT, pSideF, pSideS);
+  } else {
+    // TODO: Error Handling.
+  }
+}
--- src/triangle.h
+++ src/triangle.h
@@ -3,6 +3,18 @@
 
 class Triangle
 {
+public:
+  enum TrAngles { eANGLEA, eANGLEB, eANGLEC };
+public:
+  Triangle();
+  Triangle(double pSideA, double pSideB, double pSideC);
+  Triangle(double pSideF, double pSideS, double pAngle, TrAngles pTrAngle);
+private:
+  double m_sideA;
+  double m_sideB;
+  double m_sideC;
+private:
+  double GetSideByTwoSidesAngle(double pSideA, double pSideB, double pAngle);
 };
 
 #endif

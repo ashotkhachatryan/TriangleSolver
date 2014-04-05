52866ebf3eb0f5055e9ddc19ddb4564b4214194d
ashot <khachatryanashot1@gmail.com>
2014-04-05 18:30:06 +0400

    Implemented getter, setter functions in triangle class

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -1,4 +1,244 @@
 #include "triangle.h"
+#include "math.h"
+
+void
+Triangle::SetArea(void)
+{
+  // The Heron formula s=sqrt(p(p-a)(p-b)(p-c))
+  m_area = sqrt((GetPerimeter()/2) *
+                ((GetPerimeter()/2) - m_sideA) *
+                ((GetPerimeter()/2) - m_sideB) *
+                ((GetPerimeter()/2) - m_sideC));
+}
+
+double
+Triangle::SetAngle(double pSideX, double pSideY, double pSideZ)
+{
+  // asin(2S/ab)
+  double lAngle = (180 * asin((2 * m_area) / (pSideX * pSideY)))/PI;
+  if ((pSideZ * pSideZ) > ((pSideX * pSideX) + (pSideY * pSideY)))
+    return lAngle + ((90-lAngle)*2);
+  else 
+    return lAngle;
+}
+
+double
+Triangle::SetAltitude(double pSide)
+{
+  // 2*S/a;
+  return (2 * GetArea())/pSide;
+}
+
+double
+Triangle::SetBisector(double pSideX, double pSideY, double pAngle)
+{
+  // 2*b*c*cos(C/2)/(b+c)
+  return  2 * pSideX  * pSideY * 
+          cos(((PI * (pAngle / 2)) / 180)) / 
+          (pSideX + pSideY);
+}
+
+double
+Triangle::SetMedian(double pSide, double pSideX, double pSideY)
+{
+  // sqrt(2b^2+2c^2-a^2)/2;
+  return (sqrt(2 * pSideX * pSideX + 
+              2 * pSideY * pSideY - 
+              pSide * pSide)) / 2;
+}
+
+double
+Triangle::GetArea(void)
+{
+  return m_area;
+}
+
+void
+Triangle::SetAngleA(void)
+{
+ m_angleA = SetAngle(m_sideA, m_sideC, m_sideB); 
+}
+
+double
+Triangle::GetAngleA(void)
+{
+  return m_angleA;
+}
+
+void
+Triangle::SetAngleB(void)
+{
+ m_angleB = SetAngle(m_sideA, m_sideB, m_sideC); 
+}
+
+double
+Triangle::GetAngleB(void)
+{
+  return m_angleB;
+}
+
+void
+Triangle::SetAngleC(void)
+{
+ m_angleC = SetAngle(m_sideB, m_sideC, m_sideA); 
+}
+
+double
+Triangle::GetAngleC(void)
+{
+  return m_angleC;
+}
+
+void
+Triangle::SetPerimeter(void)
+{
+  m_perimeter = m_sideA + m_sideB + m_sideC;
+}
+
+double
+Triangle::GetPerimeter(void)
+{
+  return m_perimeter;
+}
+
+
+void
+Triangle::SetAltitudeA(void)
+{
+  m_altitudeA = SetAltitude(m_sideA); 
+}
+
+double
+Triangle::GetAltitudeA(void)
+{
+  return m_altitudeA;
+}
+
+void
+Triangle::SetAltitudeB(void)
+{
+  m_altitudeA = SetAltitude(m_sideB); 
+}
+
+double
+Triangle::GetAltitudeB(void)
+{
+  return m_altitudeB;
+}
+
+void
+Triangle::SetAltitudeC(void)
+{
+  m_altitudeA = SetAltitude(m_sideC);
+}
+
+double
+Triangle::GetAltitudeC(void)
+{
+  return m_altitudeC;
+}
+
+void
+Triangle::SetBisectorA(void)
+{
+  m_bisectorA = SetBisector(m_sideB, m_sideC, m_angleC);
+}
+
+double
+Triangle::GetBisectorA(void)
+{
+  return m_bisectorA;
+}
+
+void
+Triangle::SetBisectorB(void)
+{
+  m_bisectorA = SetBisector(m_sideA, m_sideC, m_angleA);
+}
+
+double
+Triangle::GetBisectorB(void)
+{
+  return m_bisectorB;
+}
+
+void
+Triangle::SetBisectorC(void)
+{
+  m_bisectorA = SetBisector(m_sideA, m_sideB, m_angleB);
+}
+
+double
+Triangle::GetBisectorC(void)
+{
+  return m_bisectorC;
+}
+
+void
+Triangle::SetMedianA(void)
+{
+  m_medianA = SetMedian(m_sideA, m_sideB, m_sideC);
+}
+
+double
+Triangle::GetMedianA(void)
+{
+  return m_medianA;
+}
+
+void
+Triangle::SetMedianB(void)
+{
+  m_medianA = SetMedian(m_sideB, m_sideA, m_sideC);
+}
+
+double
+Triangle::GetMedianB(void)
+{
+  return m_medianB;
+}
+
+void
+Triangle::SetMedianC(void)
+{
+  m_medianA = SetMedian(m_sideC, m_sideB, m_sideA);
+}
+
+double
+Triangle::GetMedianC(void)
+{
+  return m_medianC;
+}
+
+void
+Triangle::SetInscribedR(void)
+{
+  m_inscribedR = GetArea() / (GetPerimeter()/2);
+}
+
+void
+Triangle::SetOutscribedR(void)
+{
+  // (abc)/sqrt(a+b+c)(b+c-a)(c+a-b)(a+b-c)
+  m_outscribedR = (m_sideA * m_sideB * m_sideC) / 
+          sqrt(
+          (m_sideA + m_sideB + m_sideC) * 
+          (m_sideB + m_sideC - m_sideA) * 
+          (m_sideC + m_sideA - m_sideB) * 
+          (m_sideA + m_sideB - m_sideC));
+}
+
+double
+Triangle::GetInscribedR(void)
+{
+  return m_inscribedR;
+}
+
+double
+Triangle::GetOutscribedR(void)
+{
+  return m_outscribedR;
+}
 
 double
 Triangle::GetSideByTwoSidesAngle(double pSideA, double pSideB, double pAngle)
@@ -19,6 +259,11 @@ Triangle::Triangle(double pSideA, double pSideB, double pSideC)
   m_sideA = pSideA;
   m_sideB = pSideB;
   m_sideC = pSideC;
+  SetPerimeter();
+  SetArea();
+  SetAngleA();
+  SetAngleB();
+  SetAngleC();
 }
 
 Triangle::Triangle(double pSideF, double pSideS, double pAngle, TrAngles pTrAngle)
--- src/triangle.h
+++ src/triangle.h
@@ -1,6 +1,8 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
+#define PI 3.1415926535897
+
 class Triangle
 {
 public:
@@ -13,8 +15,66 @@ private:
   double m_sideA;
   double m_sideB;
   double m_sideC;
+  double m_angleA;
+  double m_angleB;
+  double m_angleC;
+  double m_perimeter;
+  double m_area;
+  double m_altitudeA;
+  double m_altitudeB;
+  double m_altitudeC;
+  double m_bisectorA;
+  double m_bisectorB;
+  double m_bisectorC;
+  double m_medianA;
+  double m_medianB;
+  double m_medianC;
+  double m_inscribedR;
+  double m_outscribedR;
 private:
+  void SetArea();
+  void SetPerimeter();
+  double SetAltitude(double pSide);
+  void SetAltitudeA();
+  void SetAltitudeB();
+  void SetAltitudeC();
+  double SetBisector(double pSideX, double pSideY, double pAngle);
+  void SetBisectorA();
+  void SetBisectorB();
+  void SetBisectorC();
+  double SetMedian(double pSide, double pSideX, double pSideY);
+  void SetMedianA();
+  void SetMedianB();
+  void SetMedianC(void);
+  double SetAngle(double pSideX, double pSideY, double pSideZ);
+  void SetAngleA();
+  void SetAngleB();
+  void SetAngleC();
+  void SetInscribedR();
+  void SetOutscribedR();
   double GetSideByTwoSidesAngle(double pSideA, double pSideB, double pAngle);
+public:
+  double GetArea();
+  double GetPerimeter();
+
+  double GetAltitudeA();
+  double GetAltitudeB();
+  double GetAltitudeC();
+
+  double GetBisectorA();
+  double GetBisectorB();
+  double GetBisectorC();
+
+  double GetMedianA();
+  double GetMedianB();
+  double GetMedianC();
+
+  double GetAngleA();
+  double GetAngleB();
+  double GetAngleC();
+
+  double GetInscribedR();
+  double GetOutscribedR();
 };
 
 #endif

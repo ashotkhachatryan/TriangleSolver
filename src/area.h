1d0a59f4137004b9f5f9d43a65aba461d33721ad
ashot <khachatryanashot1@gmail.com>
2014-04-12 17:44:22 +0400

    Added missed area.h file

---
+++ src/area.h
@@ -0,0 +1,16 @@
+#ifndef AREA_H
+#define AREA_H
+
+struct Area
+{
+  double X;
+  double Y;
+  double height;
+  double width;
+  double topMargin;
+  double bottomMargin;
+  double leftMargin;
+  double rightMargin;
+};
+
+#endif
